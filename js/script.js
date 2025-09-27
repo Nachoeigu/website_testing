e3859723df35a95350cc0f7b44cc21194e1fb99a
Nacho Eigu 🇦🇷 <ignacio.eiguren@gmail.com>
2025-09-27 10:33:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -45,3 +45,39 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 });
+
+// --- LÓGICA DEL CARRUSEL DE TESTIMONIOS ---
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    grabCursor: true,
+    
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    }
+});
